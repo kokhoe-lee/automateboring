1ac17a82cd3d1122c8261860c140c37abb131e2c
upstore <upstore8@gmail.com>
2021-10-16 11:23:17 +0800

    first code

---
+++ betterhello.py
@@ -0,0 +1,12 @@
+# this program says hello and asks for your name
+
+print ("Hello pyWorld!")
+print ("Whatsyourname?")
+myName = input()
+print ("It's good to meet you, "+ myName)
+print ("The length of your name is:")
+print (len(myName))
+
+print ("Whats your age?")
+myAge = input()
+print ("You will be "+ str(int(myAge)+1) + " next year!")
